a3228c6c412b67b8482154bbfe18ebabe6da2b5e
jyothsnam@309 <jyothsnam309@gmail.com>
2022-09-03 23:38:50 +0530

    eip

--- stage-vpc.tf
+++ stage-vpc.tf
@@ -74,9 +74,9 @@ resource "aws_subnet" "stage-data" {
 
 #create eip
 
-#resource "aws_eip" "stage-eip" {
-#  vpc = true
-#}
+resource "aws_eip" "stage-eip" {
+  vpc = true
+}
 
 #create nat_gateway in pub sub
 
